e24c1953168d0e889e40a6af81fe1a5c9c12e554
hitagi64 <121134722+hitagi64@users.noreply.github.com>
2022-05-20 23:12:54 +0200

    ContentPreviewer now has rotatable scene and a grid and also some handy classes.

--- ContentPreviewer.cpp
+++ ContentPreviewer.cpp
@@ -1,6 +1,8 @@
 #include "ContentPreviewer.h"
 #include <cmath>
 #include <QTimer>
+#include <QMouseEvent>
+#include <iostream>
 
 ContentPreviewer::ContentPreviewer()
 {
@@ -8,13 +10,20 @@ ContentPreviewer::ContentPreviewer()
     connect(timer, &QTimer::timeout, this, QOverload<>::of(&ContentPreviewer::update));
     timer->start(10);
 
-    projectionMode2DImage = true;
+    projectionMode2DImage = false;
+
+    cameraRotH = 0;
+    cameraRotV = 0;
+
+    distanceFromCamera = 10;
 }
 
 void ContentPreviewer::initializeGL()
 {
     QOpenGLWidget::initializeGL();
     initializeOpenGLFunctions();
+
+    glEnable(GL_DEPTH_TEST);
 
     float vertices[] = {
         -1.0f, -1.0f, 0.0f,
@@ -27,33 +36,16 @@ void ContentPreviewer::initializeGL()
          0.0f, 0.0f, 1.0f
     };
 
-    basicShaderProgram = new QOpenGLShaderProgram(QOpenGLContext::currentContext());
-
-    QOpenGLShader vertex(QOpenGLShader::Vertex);
-    vertex.compileSourceFile("Res/vertex.glsl");
-    basicShaderProgram->addShader(&vertex);
-
-    QOpenGLShader fragment(QOpenGLShader::Fragment);
-    fragment.compileSourceFile("Res/fragment.glsl");
-    basicShaderProgram->addShader(&fragment);
-
+    basicShaderProgram = makeShaderProgram("Res/vertex.glsl", "Res/fragment.glsl");
     basicShaderProgram->link();
     basicShaderProgram->bind();
 
-    unsigned int vao;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
+    triangle = createModel(vertices, sizeof(vertices), MODELTYPE_VERTEX3_COLOR3);
 
-    unsigned int vbo;
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    std::vector<float> gridData = generateGrid(20, 20, 1);
+    grid = createModel(&gridData[0], gridData.size()*sizeof(float), MODELTYPE_VERTEX3_COLOR3);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0);
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
-    glEnableVertexAttribArray(1);
+    glPrintError();
 }
 
 void ContentPreviewer::resizeGL(int w, int h)
@@ -81,7 +73,11 @@ void ContentPreviewer::paintGL()
     if (projectionMode2DImage)
         view.lookAt(QVector3D(0, 0, -2), QVector3D(0, 0, 0), QVector3D(0, 1, 0));
     else
-        view.lookAt(QVector3D(0, 0, -2), QVector3D(0, 0, 0), QVector3D(0, 1, 0));
+        view.lookAt(QVector3D(
+                        sin(cameraRotH * (3.1415f/180))*-distanceFromCamera*cos(cameraRotV * (3.1415f/180)),
+                        sin(cameraRotV * (3.1415f/180))*-distanceFromCamera,
+                        cos(cameraRotH * (3.1415f/180))*-distanceFromCamera*cos(cameraRotV * (3.1415f/180))
+                        ), QVector3D(0, 0, 0), QVector3D(0, 1, 0));
 
     QMatrix4x4 model;
     model.translate(0, 0, 0);
@@ -92,5 +88,167 @@ void ContentPreviewer::paintGL()
 
     basicShaderProgram->setUniformValue("mvp", mvp);
 
+    useModel(triangle);
     glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    useModel(grid);
+    glDrawArrays(GL_LINES, 0, 21*21);
+}
+
+void ContentPreviewer::mousePressEvent(QMouseEvent *event)
+{
+    lastMousePos = event->pos();
+}
+
+void ContentPreviewer::mouseMoveEvent(QMouseEvent *event)
+{
+    int dx = event->x() - lastMousePos.x();
+    int dy = event->y() - lastMousePos.y();
+
+    if (event->buttons() & Qt::LeftButton)
+    {
+        cameraRotH -= dx/2.0f;
+
+        cameraRotV -= dy/2.0f;
+        if (cameraRotV < -90)
+            cameraRotV = -90;
+        if (cameraRotV > 90)
+            cameraRotV = 90;
+    }
+
+    lastMousePos = event->pos();
+}
+
+void ContentPreviewer::wheelEvent(QWheelEvent *event)
+{
+    distanceFromCamera += event->angleDelta().y()/-30.0f;
+    if (distanceFromCamera < 0.2)
+        distanceFromCamera = 0.2;
+}
+
+std::vector<float> ContentPreviewer::generateGrid(int width, int height, float spacing)
+{
+    std::vector<float> grid;
+
+    for (int i = width/-2; i <= width/2; i++)
+    {
+        grid.push_back((height/2)*spacing);
+        grid.push_back(0);
+        grid.push_back(i*spacing);
+
+        grid.push_back(0.5f);
+        grid.push_back(0.5f);
+        grid.push_back(0.5f);
+
+        grid.push_back((height/-2)*spacing);
+        grid.push_back(0);
+        grid.push_back(i*spacing);
+
+        grid.push_back(0.5f);
+        grid.push_back(0.5f);
+        grid.push_back(0.5f);
+    }
+
+    for (int i = height/-2; i <= height/2; i++)
+    {
+        grid.push_back(i*spacing);
+        grid.push_back(0);
+        grid.push_back((width/2)*spacing);
+
+        grid.push_back(0.5f);
+        grid.push_back(0.5f);
+        grid.push_back(0.5f);
+
+        grid.push_back(i*spacing);
+        grid.push_back(0);
+        grid.push_back((width/-2)*spacing);
+
+        grid.push_back(0.5f);
+        grid.push_back(0.5f);
+        grid.push_back(0.5f);
+    }
+
+    return grid;
+}
+
+ModelData ContentPreviewer::createModel(void *data, unsigned int dataSize, ModelType type)
+{
+    ModelData model;
+
+    glGenVertexArrays(1, &model.vao);
+
+    model.vbo = makeVBO(data, dataSize);
+
+    glBindVertexArray(model.vao);
+
+    if (type == MODELTYPE_VERTEX3_COLOR3)
+    {
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0);
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+        glEnableVertexAttribArray(1);
+    }
+    else if (type == MODELTYPE_VERTEX3_UV2)
+    {
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0);
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3*sizeof(float)));
+        glEnableVertexAttribArray(1);
+    }
+
+
+    return model;
+}
+
+void ContentPreviewer::useModel(ModelData model)
+{
+    glBindVertexArray(model.vao);
+}
+
+void ContentPreviewer::deleteModel(ModelData model)
+{
+    glDeleteVertexArrays(1, &model.vao);
+    deleteVBO(model.vbo);
+}
+
+QOpenGLShaderProgram *ContentPreviewer::makeShaderProgram(QString vertexPath, QString fragmentPath)
+{
+    QOpenGLShaderProgram *p = new QOpenGLShaderProgram(QOpenGLContext::currentContext());
+
+    QOpenGLShader vertex(QOpenGLShader::Vertex);
+    vertex.compileSourceFile(vertexPath);
+    p->addShader(&vertex);
+
+    QOpenGLShader fragment(QOpenGLShader::Fragment);
+    fragment.compileSourceFile(fragmentPath);
+    p->addShader(&fragment);
+
+    return p;
+}
+
+unsigned int ContentPreviewer::makeVBO(void *data, unsigned int dataSize)
+{
+    unsigned int vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, dataSize, data, GL_STATIC_DRAW);
+
+    return vbo;
+}
+
+void ContentPreviewer::useVBO(unsigned int vbo)
+{
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+}
+
+void ContentPreviewer::deleteVBO(unsigned int vbo)
+{
+    glDeleteBuffers(1, &vbo);
+}
+
+void ContentPreviewer::glPrintError()
+{
+    std::cout << glGetError() << std::endl;
 }
--- ContentPreviewer.h
+++ ContentPreviewer.h
@@ -6,20 +6,58 @@
 #include <QOpenGLBuffer>
 #include <QOpenGLShaderProgram>
 
+enum ModelType
+{
+    MODELTYPE_VERTEX3_COLOR3,
+    MODELTYPE_VERTEX3_UV2,
+};
+
+struct ModelData
+{
+    unsigned int vao;
+    unsigned int vbo;
+    ModelType type;
+};
+
 class ContentPreviewer : public QOpenGLWidget, protected QOpenGLFunctions_3_3_Core
 {
     Q_OBJECT
 public:
     ContentPreviewer();
 protected:
-    void initializeGL();
-    void resizeGL( int w, int h );
-    void paintGL();
+    void initializeGL() override;
+    void resizeGL(int w, int h) override;
+    void paintGL() override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void wheelEvent(QWheelEvent *event) override;
 private:
+    std::vector<float> generateGrid(int width, int height, float spacing);
+
+    ModelData createModel(void *data, unsigned int dataSize, ModelType type);
+    void useModel(ModelData model);
+    void deleteModel(ModelData model);
+
     QMatrix4x4 projection;
     QOpenGLShaderProgram *basicShaderProgram;
 
     bool projectionMode2DImage;
+
+    float cameraRotH;
+    float cameraRotV;
+    float distanceFromCamera;
+
+    QPoint lastMousePos;
+
+    ModelData triangle;
+    ModelData grid;
+
+    // Opengl
+    QOpenGLShaderProgram *makeShaderProgram(QString vertexPath, QString fragmentPath);
+    unsigned int makeVBO(void *data, unsigned int dataSize);
+    void useVBO(unsigned int vbo);
+    void deleteVBO(unsigned int vbo);
+    void glPrintError();
 };
 
 #endif // CONTENTPREVIEWER_H
